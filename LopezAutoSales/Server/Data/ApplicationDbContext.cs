ac1f06b3b12d495e6fa0e81e003c8f316a61ad38
Gabe Lopez <glopez11193@gmail.com>
2020-07-13 16:59:43 -0500

    Add Files

---
+++ LopezAutoSales/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using IdentityServer4.EntityFramework.Options;
+using LopezAutoSales.Server.Models;
+using LopezAutoSales.Shared.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace LopezAutoSales.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+        }
+    }
+}
---
+++ LopezAutoSales/Shared/Models/Sale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LopezAutoSales.Shared.Models
+{
+    public class Sale
+    {
+        public string Id { get; set; }
+        public DateTime Date { get; set; }
+        public string Phone { get; set; }
+        public string Buyer { get; set; }
+        public string CoBuyer { get; set; }
+    }
+}
